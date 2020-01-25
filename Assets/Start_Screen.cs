24ce1b76462e29147392fafbddfb863688fcf082
tjepster <jeppevidw@gmail.com>
2020-01-25 15:25:09 +0100

    start screen

---
+++ Assets/Start_Screen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Screen : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Debug.Log("ok");
+            int scene = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
