426b49c955ba53f195eb519be1ac2dc68a5d3072
tjepster <jeppevidw@gmail.com>
2020-01-28 16:38:24 +0100

    Update OptionMenu.cs

--- Assets/scripts/OptionMenu.cs
+++ Assets/scripts/OptionMenu.cs
@@ -20,13 +20,22 @@ public class OptionMenu : MonoBehaviour
 
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
+
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
     
     // Here we change the overal volume of the game
@@ -39,7 +48,13 @@ public class OptionMenu : MonoBehaviour
         QualitySettings.SetQualityLevel(qualityIndex);
     }
 
-    public void FullScreen(bool full)
+    public void SetResolution(int resulutionIndex)
+    {
+        Resolution resolution = resolutions[resulutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    
+        public void FullScreen(bool full)
     {
         Screen.fullScreen = full;
     }
